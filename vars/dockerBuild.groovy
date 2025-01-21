257b0869d9ba7a63fe66d261c3b80a10010ca625
singhraju <singh.raju@msn.com>
2025-01-21 10:42:46 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,11 @@
-// Define methods for Docker-related tasks
-
-def login(String credentialsId = '391e5063-438b-4b7b-b98a-e46b7211ea3e') {
+// vars/dockerBuild.groovy
+def login(String credentialsId = 'docker-credentials') {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
+        sh 'docker login -u singhraju -p $DOCKER_PASS'
     }
 }
 
-def build(String imageName, String context) {
+def build(String imageName, String context = '.') {
     sh "docker build -t ${imageName} ${context}"
 }
 
