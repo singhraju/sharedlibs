89d714e5a47cd2e25ffa95d04f388dc284b8949d
singhraju <singh.raju@msn.com>
2025-01-21 10:54:12 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,15 +1,13 @@
-// vars/dockerBuild.groovy
 def login(String credentialsId = 'docker-secret') {
     withCredentials([string(credentialsId: credentialsId, variable: 'DOCKER_TOKEN')]) {
-        // For Docker Hub or similar registries
-        sh "echo $DOCKER_TOKEN | docker login -u singhraju --password-stdin"
+        bat "echo %DOCKER_TOKEN% | docker login -u singhraju --password-stdin"
     }
 }
 
 def build(String imageName, String context = '.') {
-    sh "docker build -t ${imageName} ${context}"
+    bat "docker build -t ${imageName} ${context}"
 }
 
 def push(String imageName) {
-    sh "docker push ${imageName}"
+    bat "docker push ${imageName}"
 }
